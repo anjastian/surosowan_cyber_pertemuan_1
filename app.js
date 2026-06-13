5825ca60e39fe6efa9897eb414c3483b36a59b9c
anjastian <anjastian@gmail.com>
2026-06-13 16:58:36 +0700

    update progress pertemuan ke 2

---
+++ app.js
@@ -0,0 +1,22 @@
+let A = 50
+let B = 10
+let C = 5
+let D = 30
+
+console.log((A * C) + D);
+console.log((B + C) - D);
+console.log((A / C) + B);
+
+let nilai = 80
+console.log(nilai > 75)
+console.log(nilai < 75)
+console.log(nilai == 75)
+console.log(nilai !== 75)
+
+
+let username = true;
+let password = true;
+
+console.log(username && password)
+console.log(username || password)
+console.log(username);
